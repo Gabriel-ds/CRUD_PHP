37b2d32b7a9d9720057598c7fb7d44aadfae8199
Gabriel dos Santos <gabrields1910@gmail.com>
2021-08-31 22:07:28 -0300

    Metodo create

--- display.php
+++ display.php
@@ -1,3 +1,7 @@
+<?php
+    include 'connect.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,6 +13,66 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <button class="btn btn-primary my-5">
+            <a href="user.php" class="text-light" style="text-decoration: none;">Adicionar usuário</a>
+        </button>
+
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Sl no</th>
+      <th scope="col">Nome</th>
+      <th scope="col">E-mail</th>
+      <th scope="col">Telefone</th>
+      <th scope="col">Senha</th>
+      <th scope="col">Ação</th>
+    </tr>
+  </thead>
+  <tbody>
+
+
+  <?php
+
+    $sql="Select * from `crud`";
+    $result = mysqli_query($con,$sql);
+    if($result){
+        while($row = mysqli_fetch_assoc($result)){
+            $id = $row['id'];
+            $name = $row['name'];
+            $email = $row['email'];
+            $mobile = $row['mobile'];
+            $password = $row['password'];
+            echo '<tr>
+            <th scope="row">'.$id.'</th>
+            <td>'.$name.'</td>
+            <td>'.$email.'</td>
+            <td>'.$mobile.'</td>
+            <td>'.$password.'</td>
+          </tr>';
+        }
+    }
+
+  ?>
+    <!-- <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr> -->
+  </tbody>
+</table>
+    </div>
 </body>
 </html>
